cfb92086b2fb3a524c2263e04f14199b66017dca
Abhilasha75 <abhilasha.jadhav92@gmail.com>
2021-05-02 13:52:23 +0530

    Initial commit

---
+++ app/src/main/java/com/abhilasha/androidclass/splashactivity/splashactivity.java
@@ -0,0 +1,34 @@
+package com.abhilasha.androidclass.splashactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splashactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(7000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(splashactivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+
+}
